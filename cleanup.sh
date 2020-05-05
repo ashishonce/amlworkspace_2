673baa8a1ed90dfd50a2d1434bc5af882a3a08b2
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-05-05 12:57:02 +0530

    adding test shell script to run

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-id=$1
-rg=$2
-# Create a resource group.
-echo $id
-echo $rg
-az ml workspace list
-# az ml workspace delete -id "" -g aml_workspace_intTest_rg -w aml_workspace_intTest
+az extension add -n azure-cli-ml
+az ml workspace list --resource-group ashkuma_devtesting
