ae98b76d4799baa85af9884655c419f7774f234e
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-05-05 14:21:10 +0530

    adding test shell script to run

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,6 @@
 sid=$1
 echo "$sid"
 #extract the resource group
-jq.resource_group /.cloud/.azure/test/workspace.json
+jq .resource_group /.cloud/.azure/test/workspace.json
 # az extension add -n azure-cli-ml
 # az ml workspace list --resource-group "$rg"
