e34e1e78d5c25abe4fa3343ef73cec06a4ea4df9
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-05-05 17:45:32 +0530

    fake crd test

--- cleanup.sh
+++ cleanup.sh
@@ -4,8 +4,8 @@
 # rg=$(jq .resource_group .cloud/.azure/test/workspace.json)
 rg=ashkuma_devtesting
 ws=ashkumamlworkspace
-echo $rg
 echo $ws
+echo $rg
 jq -n "$1" | jq ."clientId"
 # az extension add -n azure-cli-ml
 # az ml workspace delete --subscription $sid --resource-group $rg --workspace-name $ws
