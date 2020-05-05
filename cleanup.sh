a052ce5fbe934bca6ce2fa35d33e2d52eed3dd12
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-05-05 16:36:18 +0530

    adding support to delete a ws

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,8 @@ sid=$1
 echo $sid
 #extract the resource group
 rg=$(jq .resource_group .cloud/.azure/test/workspace.json)
-# rg=ashkuma_devtesting
+rg=ashkuma_devtesting
+ws=ashkumamlworkspace
 echo $rg
 az extension add -n azure-cli-ml
-az ml workspace list --resource-group $rg
+az ml workspace delete --subscription $sid --resource-group $rg --workspace-name $ws
