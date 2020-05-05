2d2fd648a2df8268f3517860461f998830445733
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-05-05 13:53:15 +0530

    adding test shell script to run

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $1
 # apt install jq
-jq .resource_group .cloud/.azure/test/workspace.json
-# az extension add -n azure-cli-ml
-# az ml workspace list --resource-group 
+rg = jq .resource_group .cloud/.azure/test/workspace.json
+az extension add -n azure-cli-ml
+az ml workspace list --resource-group $rg
