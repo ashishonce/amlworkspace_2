412661212d154cabd57db398663ce05a46349c55
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-05-05 13:45:48 +0530

    adding test shell script to run

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $1
-apt install jq
-jq .subscriptionId $1
-az extension add -n azure-cli-ml
-az ml workspace list --resource-group ashkuma_devtesting
+# apt install jq
+# jq .subscriptionId $1
+# az extension add -n azure-cli-ml
+# az ml workspace list --resource-group ashkuma_devtesting
